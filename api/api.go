a2a99fb62ce593a005152c04ad1064f4cd2f0e80
Jason Parrott <fanatic+github@gmail.com>
2017-09-16 10:39:33 -0400

    Pretty much working

--- api/api.go
+++ api/api.go
@@ -1,6 +1,14 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/maruel/panicparse/stack"
+)
 
 func CreateRouter() *gin.Engine {
 	r := gin.Default()
@@ -9,5 +17,65 @@ func CreateRouter() *gin.Engine {
 			"status": "success",
 		})
 	})
+
+	r.Static("/public", "public")
+	r.LoadHTMLGlob("api/templates/*")
+	r.GET("/", index)
+	r.POST("/", upload)
 	return r
+}
+
+func index(c *gin.Context) {
+	render(c, gin.H{
+		"title": "Main website",
+	})
+}
+
+var defaultPalette = stack.Palette{
+	EOLReset:               "</span>",
+	RoutineFirst:           `<span class="has-text-info has-text-weight-bold">`,
+	CreatedBy:              `<span class="has-text-grey">`,
+	Package:                `<span class="has-text-weight-bold")`,
+	SourceFile:             "</span>",
+	FunctionStdLib:         `<span class="has-text-success">`,
+	FunctionStdLibExported: `<span class="has-text-success has-text-weight-bold">`,
+	FunctionMain:           `<span class="has-text-primary has-text-weight-bold">`,
+	FunctionOther:          `<span class="has-text-danger">`,
+	FunctionOtherExported:  `<span class="has-text-danger has-text-weight-bold">`,
+	Arguments:              "</span>",
+}
+
+func upload(c *gin.Context) {
+	rawPanic := c.PostForm("text")
+	fullPath := false
+	p := defaultPalette
+	s := stack.AnyValue // stack.AnyPointer
+	in := strings.NewReader(rawPanic)
+	out := ioutil.Discard
+	h := gin.H{
+		"rawPanic": rawPanic,
+	}
+	goroutines, err := stack.ParseDump(in, out)
+	if err != nil {
+		h["error"] = "Internal error: " + err.Error()
+		render(c, h)
+		return
+	}
+
+	h["showGotracebackTip"] = len(goroutines) == 1
+
+	buckets := stack.SortBuckets(stack.Bucketize(goroutines, s))
+	srcLen, pkgLen := stack.CalcLengths(buckets, fullPath)
+	htmlBuckets := []string{}
+	for _, bucket := range buckets {
+		htmlBuckets = append(htmlBuckets, p.BucketHeader(&bucket, fullPath, len(buckets) > 1))
+		htmlBuckets = append(htmlBuckets, p.StackLines(&bucket.Signature, srcLen, pkgLen, fullPath))
+	}
+	h["buckets"] = template.HTML(strings.Join(htmlBuckets, ""))
+
+	render(c, h)
+}
+
+func render(c *gin.Context, h gin.H) {
+	c.HTML(http.StatusOK, "index.tmpl", h)
 }
